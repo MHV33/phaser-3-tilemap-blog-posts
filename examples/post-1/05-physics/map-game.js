d65d0043b753ed9994fc74aefe6142fe321f8b21
Andrew <avanderheyden@gmail.com>
2020-01-05 21:40:23 -0600

    click to move working

--- examples/post-1/05-physics/map-game.js
+++ examples/post-1/05-physics/map-game.js
@@ -43,7 +43,7 @@ function preload() {
 
 function create() {
   let map = this.make.tilemap({ key: "map" });
-  game.map = map;
+
 
   // Handles the clicks on the map to make the character move
   this.input.on('pointerup', game.handleClick);
@@ -63,6 +63,8 @@ function create() {
   // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
   // Higher depths will sit on top of lower depth objects.
   aboveLayer.setDepth(10);
+
+  game.map = map;
 
   // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
   // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
@@ -126,10 +128,7 @@ function create() {
     repeat: -1
   });
 
-  const camera = this.cameras.main;
-  camera.startFollow(player);
-  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  game.camera = camera;
+
 
   cursors = this.input.keyboard.createCursorKeys();
 
@@ -165,6 +164,7 @@ function create() {
   game.marker = this.add.graphics();
   game.marker.lineStyle(3, 0xffffff, 1);
   game.marker.strokeRect(0, 0, game.map.tileWidth, game.map.tileHeight);
+  game.marker.setDepth(15);
 
   // ### Pathfinding stuff ###
   // Initializing the pathfinder
@@ -172,9 +172,9 @@ function create() {
 
   // We create the 2D array representing all the tiles of our map
   var grid = [];
-  for (var y = 0; y < game.map.height; y++) {
+  for(var y = 0; y < game.map.height; y++) {
     var col = [];
-    for (var x = 0; x < game.map.width; x++) {
+    for(var x = 0; x < game.map.width; x++) {
       // In each cell we store the ID of the tile, which corresponds
       // to its index in the tileset of the map ("ID" field in Tiled)
       col.push(game.getTileID(x, y));
@@ -183,24 +183,30 @@ function create() {
   }
   game.finder.setGrid(grid);
 
-  debugger;
+
   var tileset2 = game.map.tilesets[0];
   var properties = tileset2.tileProperties;
   var acceptableTiles = [];
 
   // We need to list all the tile IDs that can be walked on. Let's iterate over all of them
   // and see what properties have been entered in Tiled.
-  //debugger;
-  for (var i = tileset2.firstgid - 1; i < tileset.total; i++) { // firstgid and total are fields from Tiled that indicate the range of IDs that the tiles can take in that tileset
-    if (!properties.hasOwnProperty(i)) {
+
+  for(var i = tileset2.firstgid - 1; i < tileset.total; i++) { // firstgid and total are fields from Tiled that indicate the range of IDs that the tiles can take in that tileset
+    if(!properties.hasOwnProperty(i)) {
       // If there is no property indicated at all, it means it's a walkable tile
       acceptableTiles.push(i + 1);
       continue;
     }
-    if (!properties[i].collides) acceptableTiles.push(i + 1);
-    if (properties[i].cost) game.finder.setTileCost(i + 1, properties[i].cost); // If there is a cost attached to the tile, let's register it
+    if(!properties[i].collides) acceptableTiles.push(i + 1);
+    if(properties[i].cost) game.finder.setTileCost(i + 1, properties[i].cost); // If there is a cost attached to the tile, let's register it
   }
   game.finder.setAcceptableTiles(acceptableTiles);
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
+  //camera.startFollow(game.marker);
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  game.camera = camera;
 }
 
 function update(time, delta) {
@@ -208,19 +214,19 @@ function update(time, delta) {
   const prevVelocity = player.body.velocity.clone();
 
   // Stop any previous movement from the last frame
-  //player.body.setVelocity(0);
+  player.body.setVelocity(0);
 
   // Horizontal movement
-  if (cursors.left.isDown) {
+  if(cursors.left.isDown) {
     player.body.setVelocityX(-speed);
-  } else if (cursors.right.isDown) {
+  } else if(cursors.right.isDown) {
     player.body.setVelocityX(speed);
   }
 
   // Vertical movement
-  if (cursors.up.isDown) {
+  if(cursors.up.isDown) {
     player.body.setVelocityY(-speed);
-  } else if (cursors.down.isDown) {
+  } else if(cursors.down.isDown) {
     player.body.setVelocityY(speed);
   }
 
@@ -228,22 +234,22 @@ function update(time, delta) {
   player.body.velocity.normalize().scale(speed);
 
   // Update the animation last and give left/right animations precedence over up/down animations
-  if (cursors.left.isDown) {
+  if(cursors.left.isDown) {
     player.anims.play("misa-left-walk", true);
-  } else if (cursors.right.isDown) {
+  } else if(cursors.right.isDown) {
     player.anims.play("misa-right-walk", true);
-  } else if (cursors.up.isDown) {
+  } else if(cursors.up.isDown) {
     player.anims.play("misa-back-walk", true);
-  } else if (cursors.down.isDown) {
+  } else if(cursors.down.isDown) {
     player.anims.play("misa-front-walk", true);
   } else {
     player.anims.stop();
 
     // If we were moving, pick and idle frame to use
-    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
-    else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
-    else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
-    else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+    if(prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+    else if(prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+    else if(prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+    else if(prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
   }
 
   //used to set cursor marker
@@ -258,15 +264,18 @@ function update(time, delta) {
 }
 
 game.checkCollision = function (x, y) {
-  var tile = game.map.getTileAt(x, y);
-  if (tile == null) return false;
+
+  var tile = game.map.getTileAt(x, y, false, "World");
+  if(tile == null) return false;
   return tile.properties.collides == true;
 };
 
 game.getTileID = function (x, y) {
-  //debugger;
-  var tile = game.map.getTileAt(x, y);
-  if (tile == null) return 0;
+
+  var tile = game.map.getTileAt(x, y, false, "World");
+  if(tile == null) {
+    tile = game.map.getTileAt(x, y, false, "Below Player");
+  }
   return tile.index;
 };
 
@@ -281,7 +290,7 @@ game.handleClick = function (pointer) {
   console.log('going from (' + fromX + ',' + fromY + ') to (' + toX + ',' + toY + ')');
 
   game.finder.findPath(fromX, fromY, toX, toY, function (path) {
-    if (path === null) {
+    if(path === null) {
       console.warn("Path was not found.");
     } else {
       console.log(path);
@@ -295,7 +304,7 @@ game.moveCharacter = function (path) {
   console.log('moveCharacter()');
   // Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
   var tweens = [];
-  for (var i = 0; i < path.length - 1; i++) {
+  for(var i = 0; i < path.length - 1; i++) {
     var ex = path[i + 1].x;
     var ey = path[i + 1].y;
     tweens.push({
@@ -304,7 +313,7 @@ game.moveCharacter = function (path) {
       y: { value: ey * game.map.tileHeight, duration: 200 }
     });
   }
-  //debugger;
+
   game.currentScene.tweens.timeline({
     tweens: tweens
   });
